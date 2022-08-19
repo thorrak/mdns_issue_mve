ac3fa5118877c4421f7f3f5599c2c11f19dd6574
John <jbeeler@optictheory.com>
2022-08-19 12:20:23 -0400

    Create MVE

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+
+#include "secret.h"
+const char *ssid = WIFI_SSID;      // Defined in secret.h
+const char *password = WIFI_PASS;  // Defined in secret.h
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.flush();
+  Serial.println("Starting test");
+
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Establishing connection to WiFi..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+      delay(1000);
+      Serial.print(F("."));
+  }
+
+  // This next line is the one that seems to be required for mDNS to work, but means
+  // that WiFi and Bluetooth coexistence are not possible 
+  WiFi.setSleep(false);
+
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP().toString());
+
+  MDNS.begin(WiFi.getHostname());
+  if (!MDNS.begin("tiltbridge")) {
+    Serial.println("Error setting up MDNS responder.");
+  } else {
+      MDNS.setInstanceName("TiltBridge");
+      MDNS.addService("_tiltbridge", "_tcp", 80);
+  }
+  Serial.println("mDNS Initialized");
+}
+
+void loop()
+{
+  delay(200);
+}
